f1a150365082d973490a8ca74ab490fc868755be
viniciusfernandoss <vinicius.senac.ads@gmail.com>
2024-10-28 19:29:22 -0300

    trabalho 28/10/2024 pt2

--- js/script.js
+++ js/script.js
@@ -21,3 +21,32 @@ const swiperTwo = new Swiper('.swiper-servicos-mobile', {
       clickable: true,
     },
 });
+
+const swiper = new Swiper('.swiper-feedback', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    
+    pagination: {
+        el: '.swiper-feedback .swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+    
+    navigation: {
+        nextEl: '.swiper-feedback .swiper-button-next',
+        prevEl: '.swiper-feedback .swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        992: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+ });
